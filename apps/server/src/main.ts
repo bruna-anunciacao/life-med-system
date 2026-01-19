9e7a512aff563dd0cff73db015f8e5744cf03f95
unknown <bruunaaa04@gmail.com>
2026-01-19 16:23:46 -0300

    fix: add prod frontend url

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,11 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [process.env.PROD_FRONTEND_URL, 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  // ---------------------------
 
   app.useGlobalPipes(
     new ValidationPipe({
