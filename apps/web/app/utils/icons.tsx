12847950fa90eb5a5f084757239dfb17a5630799
unknown <bruunaaa04@gmail.com>
2026-02-19 21:21:13 -0300

    feat: create patients page in professional dashboards

--- apps/web/app/utils/icons.tsx
+++ apps/web/app/utils/icons.tsx
@@ -124,7 +124,6 @@ export const EditIcon = (props: IconSvgProps) => {
   );
 };
 
-// Ícone de Aprovar (Check)
 export const CheckIcon = (props: IconSvgProps) => (
   <svg
     aria-hidden="true"
@@ -307,5 +306,21 @@ export const PlusIcon = () => (
   >
     <line x1="12" y1="5" x2="12" y2="19"></line>
     <line x1="5" y1="12" x2="19" y2="12"></line>
+  </svg>
+);
+
+export const SearchIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="11" cy="11" r="8" />
+    <line x1="21" y1="21" x2="16.65" y2="16.65" />
   </svg>
 );
